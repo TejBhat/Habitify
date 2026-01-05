5bcc229a85b77c8ef1ce4be64e7d0107263bc895
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-05 22:18:24 +0530

    committed

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,35 +1,14 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
+import Entypo from '@expo/vector-icons/Entypo';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
+export default function TabsLayout() {
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
-    </Tabs>
+<Tabs screenOptions={{tabBarActiveTintColor:"green"}}>
+  
+<Tabs.Screen name="index" options={{title:"Home",tabBarIcon:({color})=>(<Entypo name="home" size={24} color={color} />),}}/>
+<Tabs.Screen name="Login" options={{title:"Login",tabBarIcon:({color})=>(<MaterialIcons name="login" size={24} color={color} />),}}/>
+
+</Tabs>
   );
 }
---
+++ app/auth.tsx
@@ -0,0 +1,12 @@
+import { KeyboardAvoidingView, Platform, View } from "react-native";
+import {Text,TextInput} from 'react-native-paper'
+
+
+export default function AuthScreen(){
+    return <KeyboardAvoidingView behavior={Platform.OS==="android"?"padding":"height"}>
+        <View>
+            <Text>Create Account</Text>
+            <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com"/>
+        </View>
+    </KeyboardAvoidingView>
+}
