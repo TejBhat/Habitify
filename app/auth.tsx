c6e4287c487fcd5ea9118919a936409e442e0b87
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-05 23:08:36 +0530

    changes made

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,12 +1,16 @@
 import { KeyboardAvoidingView, Platform, View } from "react-native";
-import {Text,TextInput} from 'react-native-paper'
+import {Button, Text,TextInput} from 'react-native-paper'
 
 
 export default function AuthScreen(){
+    
     return <KeyboardAvoidingView behavior={Platform.OS==="android"?"padding":"height"}>
         <View>
             <Text>Create Account</Text>
-            <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com"/>
+            <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com" mode="outlined"/>
+             <TextInput label="Password" autoCapitalize="none" keyboardType="email-address" placeholder="Your Password" mode="outlined"/>
+             <Button mode="contained">Sign Up</Button>
+              <Button mode="text">Already have an account? Sign In</Button>
         </View>
     </KeyboardAvoidingView>
 }
