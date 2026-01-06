a90cd9b21bf52bc1f7df4d0c013076b74a16b275
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-06 21:54:16 +0530

    Auth  changes

--- app/auth.tsx
+++ app/auth.tsx
@@ -1,16 +1,67 @@
-import { KeyboardAvoidingView, Platform, View } from "react-native";
-import {Button, Text,TextInput} from 'react-native-paper'
+import { useState } from "react";
+import { KeyboardAvoidingView, Platform, View,StyleSheet } from "react-native";
+import {Button, Text,TextInput,useTheme} from "react-native-paper";
 
 
 export default function AuthScreen(){
-    
-    return <KeyboardAvoidingView behavior={Platform.OS==="android"?"padding":"height"}>
-        <View>
-            <Text>Create Account</Text>
-            <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com" mode="outlined"/>
-             <TextInput label="Password" autoCapitalize="none" keyboardType="email-address" placeholder="Your Password" mode="outlined"/>
-             <Button mode="contained">Sign Up</Button>
-              <Button mode="text">Already have an account? Sign In</Button>
+    const [isSignUp, setIsSignUp]=useState<boolean>(false);
+    const [email, setEmail]=useState<string>("");
+    const [Password, setPassword]=useState<string>("");
+    const [error, setError]=useState<string|null>("");
+    const theme=useTheme();
+
+    const handleAuth=async()=>{
+            if(!email||!Password){
+            setError("Please fill in all fields");
+            return;
+            }
+            
+            if(Password.length<6){
+                setError("Passwords must be at least 6 characters long");
+                return;
+            }
+            setError(null);
+    };
+
+    const handleSwitchMode=()=>{
+        setIsSignUp((prev)=>!prev);
+    }
+    return (<KeyboardAvoidingView behavior={Platform.OS==="android"?"padding":"height"}
+    style={styles.container}>
+        <View style={styles.content}>
+            <Text style={styles.title} variant="headlineMedium">
+                {" "}
+                {isSignUp?"Create Account":"Welcome Back"}</Text>
+            <TextInput label="Email" autoCapitalize="none" keyboardType="email-address" placeholder="example@gmail.com" mode="outlined" style={styles.input} onChangeText={setEmail}/>
+             <TextInput label="Password" autoCapitalize="none" keyboardType="email-address" placeholder="Your Password" mode="outlined"style={styles.input} onChangeText={setPassword}/>
+             {error &&<Text style={{color:theme.colors.error}}>{error}</Text>}
+             <Button mode="contained" style={styles.button} onPress={handleAuth}>{isSignUp?"Sign Up":"Sign In"}</Button>
+              <Button mode="text" onPress={handleSwitchMode} style={styles.switchModeButton}>{isSignUp?"Already have an account? Sign In":"Don't have an account? Sign Up"}</Button>
         </View>
     </KeyboardAvoidingView>
+    );
 }
+const styles=StyleSheet.create({
+container:{
+    flex:1,
+    backgroundColor:"#f5f5f5"
+},
+content:{
+    flex:1,
+    padding:16,
+    justifyContent:"center",
+},
+title:{
+   textAlign:"center",
+   marginBottom:24,
+},
+input:{
+   marginBottom:16,
+},
+button:{
+   marginTop:8,
+},
+switchModeButton:{
+    marginTop:16,
+}
+});
