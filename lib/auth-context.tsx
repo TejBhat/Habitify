a787ebfa277324d87680517d978493d519f94f58
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-08 22:59:29 +0530

    Fix: auth-context

--- lib/auth-context.tsx
+++ lib/auth-context.tsx
@@ -1,25 +1,50 @@
 import  { createContext, useContext } from "react";
-import { Models } from "react-native-appwrite";
+import { ID, Models } from "react-native-appwrite";
+import { account } from "./appwrite";
 
 type AuthContextType={
-    user:Models.User<Models.Preferences>|null;
-    signUp:(email:string,password:string)=>Promise<void>;
-    signIn:(email:string,password:string)=>Promise<void>;
+    // user:Models.User<Models.Preferences>|null;
+    signUp:(email:string,password:string)=>Promise<string|null>;
+    signIn:(email:string,password:string)=>Promise<string|null>;
 
 };
 
 
 
-const AuthContext=createContext(undefined)
+const AuthContext=createContext<AuthContextType|undefined>(undefined);
 
 export function AuthProvider({children}:{children:React.ReactNode}){
-  return <></>;
+  
+
+ const signUp= async(email:string,password:string)=>{
+    try{
+       await account.create(ID.unique(),email,password)
+       await signIn(email,password);
+       return null
+    }catch(error){
+       if(error instanceof Error){
+        return error.message
+       }
+       return "An error occured during signup";
+    }
+
+ };
+  const signIn= async(email:string,password:string)=>{
+    try{
+       await account.createEmailPasswordSession(email,password);
+       return null
+    }catch(error){
+       if(error instanceof Error){
+        return error.message
+       }
+       return "An error occured during signIn";
+    }
+ };
+ return (<AuthContext.Provider value={{signUp,signIn}}>{children}</AuthContext.Provider>
+
+  );
 }
 
 export function useAuth(){
-    const context = useContext(AuthContext);
-    if (!context) {
-        throw new Error('useAuth must be used within an AuthProvider');
-    }
-    return context;
+    
 }
