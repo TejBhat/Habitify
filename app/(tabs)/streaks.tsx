1f3203d02c4ed237d8175f12f77359bffaba9f65
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-09 22:04:16 +0530

    feat : add streaks page

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -1,6 +1,10 @@
+import { useState } from "react";
 import { View , Text} from "react-native";
 
 export default function StreaksScreen(){
+    const [habits,setHabits]=useState<string>();
+    const [completedHabits,setCompletedHabits]=useState<string>();
+
     return(
        <View>
         <Text>this is streaks</Text>
