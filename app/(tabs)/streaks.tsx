8817592423911c7ab401c7123149e256fc24a8ec
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-09 22:14:51 +0530

    feat: add streaks page

--- app/(tabs)/streaks.tsx
+++ app/(tabs)/streaks.tsx
@@ -5,6 +5,10 @@ export default function StreaksScreen(){
     const [habits,setHabits]=useState<string>();
     const [completedHabits,setCompletedHabits]=useState<string>();
 
+ let  streak=0;
+ let bestStreak=0;
+ let lastDate:Date | null=null;
+
     return(
        <View>
         <Text>this is streaks</Text>
